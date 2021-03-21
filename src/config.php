6fe3de2e52ec58c37a3fb67b6a957eaa71b2d763
HIELARD Dylan <dylan.hielard@outlook.fr>
2021-03-21 20:22:58 +0100

    Fix db bug

--- src/config.php
+++ src/config.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_DSN', 'mysql:host=sql11.freemysqlhosting.net;port=3306;dbname=sql11398280; charset=utf8');
-define('DB_USERNAME', 'sql11398280');
-define('DB_PASSWORD', 'UqQ11h7xf8');
+define('DB_DSN', 'mysql:host=sql11.freemysqlhosting.net;port=3306;dbname=sql11400485; charset=utf8');
+define('DB_USERNAME', 'sql11400485');
+define('DB_PASSWORD', 'Z4WXpGXMQF');
 mb_internal_encoding('UTF-8');
 date_default_timezone_set('Europe/Paris');
 session_start();
